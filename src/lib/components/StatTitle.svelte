1d1b28451c577f31e735de7aabeec097b9ee2276
Dmytro Cheglakov <dcheglakov@gmail.com>
2024-11-17 22:01:04 +0200

    stat tile component

---
+++ src/lib/components/StatTitle.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { title, value } = $props();
+</script>
+
+<div class="rounded-lg bg-slate-950 px-8 py-6 text-slate-200">
+	<h2 class="pb-2 font-semibold uppercase tracking-wider">{title}</h2>
+	<p class="flex items-end font-mono text-8xl font-semibold xl:text-[9rem]">
+		{value}
+	</p>
+</div>
