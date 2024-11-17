cdc1063f97edf968be9f461fcbb08c3a387ef5af
Dmytro Cheglakov <dcheglakov@gmail.com>
2024-11-17 22:17:19 +0200

    mobile fixes

--- src/lib/components/StatTitle.svelte
+++ src/lib/components/StatTitle.svelte
@@ -2,9 +2,9 @@
 	let { title, value } = $props();
 </script>
 
-<div class="rounded-lg bg-slate-950 px-8 py-6 text-slate-200">
+<div class="rounded-lg bg-slate-950 px-4 py-2 text-slate-200 lg:px-6 lg:py-4">
 	<h2 class="pb-2 font-semibold uppercase tracking-wider">{title}</h2>
-	<p class="flex items-end font-mono text-8xl font-semibold xl:text-[9rem]">
+	<p class="flex items-end font-mono text-6xl font-semibold md:text-8xl xl:text-[9rem]">
 		{value}
 	</p>
 </div>
