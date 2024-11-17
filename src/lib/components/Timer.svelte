a6828f87525f82cc1e91f33468fe47fc128dad82
Dmytro Cheglakov <dcheglakov@gmail.com>
2024-11-17 22:00:51 +0200

    timer component

---
+++ src/lib/components/Timer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { timeDisplay }: { timeDisplay: string } = $props();
+</script>
+
+<div class="p-6 text-center font-mono lg:col-span-2">
+	<span class="inline-flex items-center gap-2 rounded-full bg-gray-900 px-4 py-2 text-white"
+		>{timeDisplay}</span
+	>
+</div>
