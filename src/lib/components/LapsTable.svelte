5c4e454b8e1d6a37182c67e7083db1d21edefe3f
Dmytro Cheglakov <dcheglakov@gmail.com>
2024-11-19 22:08:12 +0200

    sidebar styles

--- src/lib/components/LapsTable.svelte
+++ src/lib/components/LapsTable.svelte
@@ -5,35 +5,36 @@
 </script>
 
 {#snippet th(title: string)}
-	<th class="border-b border-gray-400 px-4 py-2 text-right text-sm font-medium">{title}</th>
+	<th class="border-b border-gray-6 px-4 py-2 text-right text-sm font-bold text-gray-12">{title}</th
+	>
 {/snippet}
 
 {#snippet td(value: number | string)}
-	<td class="border-b border-gray-400 px-4 py-2 text-right text-sm">{value}</td>
+	<td class="border-b border-gray-7 px-4 py-2 text-right text-sm">{value}</td>
 {/snippet}
 
 {#if laps.length === 0}
-	<div class="flex h-full items-center justify-center text-slate-800">
+	<div class="flex h-full items-center justify-center text-gray-11">
 		<p>Немає кіл</p>
 	</div>
 {:else}
 	<table class="m-auto w-full table-auto">
-		<thead class="sticky top-12 bg-gray-700 text-white">
+		<thead class="sticky top-16 bg-gray-5">
 			<tr>
 				{@render th('Коло')}
-				{@render th('Час на коло')}
+				{@render th('Час')}
 				{@render th('Загальний час')}
-				{@render th('Швидкість (км/год)')}
+				{@render th('Швидкість')}
 			</tr>
 		</thead>
 		<tbody>
 			{#each laps as lap, index}
 				<tr
 					class={lap.isFastest
-						? 'bg-purple-500 text-purple-100'
+						? 'bg-brand-4 text-brand-11'
 						: lap.isSlowest
-							? 'bg-red-600 text-red-100'
-							: 'bg-gray-600 even:bg-gray-500'}
+							? 'bg-danger-4 text-danger-11'
+							: 'bg-gray-4 text-gray-11 even:bg-gray-6'}
 				>
 					{@render td(laps.length - index)}
 					{@render td(lap.lapTime)}
--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
+	import LucideX from '~icons/lucide/x';
 	let { children, isOpen = $bindable(false) } = $props();
 </script>
 
 {#if isOpen}
 	<aside
-		class="fixed inset-y-0 left-0 z-50 w-full max-w-xl overflow-y-auto bg-gray-400 !text-white"
+		class="fixed inset-y-0 left-0 z-50 w-full max-w-lg overflow-y-auto bg-gray-2 shadow-lg shadow-gray-8"
 	>
-		<header
-			class="sticky top-0 flex h-12 items-center justify-between gap-4 bg-gray-400 px-4 py-2 text-gray-900"
-		>
-			<h2 class="font-bold">Кола</h2>
-			<button class="" onclick={() => (isOpen = false)}>Закрити</button>
+		<header class="sticky top-0 flex h-16 items-center justify-between gap-4 bg-gray-2 px-4">
+			<h2 class="text-gray-12">Статистика</h2>
+			<button
+				class="rounded bg-gray-4 p-2 text-gray-11 hover:bg-gray-5"
+				onclick={() => (isOpen = false)}><LucideX /></button
+			>
 		</header>
 		{@render children()}
 	</aside>
