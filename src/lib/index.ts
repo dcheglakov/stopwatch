9da45c0d678b298d553722a12e9e1a2a6c2a4051
Dmytro Cheglakov <dcheglakov@gmail.com>
2025-03-25 16:51:45 +0200

    refactor formatTime function to use const for variable declarations

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,10 +3,10 @@ export const LAP_DISTANCE_METERS = 250;
 export const TOTAL_DISTANCE_METERS = 250_000;
 
 export function formatTime(ms: number, includeHours: boolean = false) {
-	let milliseconds = Math.floor((ms % 1000) / 10);
-	let seconds = Math.floor((ms / 1000) % 60);
-	let minutes = Math.floor((ms / (1000 * 60)) % 60);
-	let hours = Math.floor(ms / (1000 * 60 * 60));
+	const milliseconds = Math.floor((ms % 1000) / 10);
+	const seconds = Math.floor((ms / 1000) % 60);
+	const minutes = Math.floor((ms / (1000 * 60)) % 60);
+	const hours = Math.floor(ms / (1000 * 60 * 60));
 
 	if (includeHours) {
 		return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}.${String(milliseconds).padStart(2, '0')}`;
