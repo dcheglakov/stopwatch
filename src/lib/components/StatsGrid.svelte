c281dc9c8a0b560a39aab8f3c2433c4368e91403
Dmytro Cheglakov <dcheglakov@gmail.com>
2025-12-21 23:22:12 +0200

    fix: time overflow

--- src/lib/components/StatsGrid.svelte
+++ src/lib/components/StatsGrid.svelte
@@ -2,6 +2,6 @@
 	let { children } = $props();
 </script>
 
-<div class="grid h-full w-full grid-cols-1 content-center">
+<div class="grid h-full w-full grid-cols-1 content-center overflow-hidden">
 	{@render children()}
 </div>
